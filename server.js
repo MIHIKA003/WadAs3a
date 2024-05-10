4ef732a0b9974c5876269fb7f9a93f67f07532b9
MIHIKA003 <107033183+MIHIKA003@users.noreply.github.com>
2024-05-11 02:38:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express =  require('express');
+const path = require('path');
+
+const app =  express();
+const port = 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
